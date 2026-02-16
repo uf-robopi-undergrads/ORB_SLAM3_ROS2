b087b5a81d40a31afaf605adf09f0ca64dfdc151
Herschenglime <herschenglime@gmail.com>
2026-02-16 21:59:54 +0000

    Create preliminary mono-inertial nodes
    Based on the mono nodes with a rename

---
+++ src/monocular-inertial/monocular-inertial-slam-node.cpp
@@ -0,0 +1,47 @@
+#include "monocular-slam-node.hpp"
+
+#include<opencv2/core/core.hpp>
+
+using std::placeholders::_1;
+
+MonocularInertialSlamNode::MonocularInertialSlamNode(ORB_SLAM3::System* pSLAM)
+:   Node("ORB_SLAM3_ROS2")
+{
+    m_SLAM = pSLAM;
+    // std::cout << "slam changed" << std::endl;
+    m_image_subscriber = this->create_subscription<ImageMsg>(
+        "/image_raw",
+        10,
+        std::bind(&MonocularInertialSlamNode::GrabImage, this, std::placeholders::_1));
+    std::cout << "slam changed" << std::endl;
+}
+
+MonocularInertialSlamNode::~MonocularInertialSlamNode()
+{
+    // Stop all threads
+    m_SLAM->Shutdown();
+
+    // Save camera trajectory
+    m_SLAM->SaveKeyFrameTrajectoryTUM("KeyFrameTrajectory.txt");
+}
+
+void MonocularInertialSlamNode::GrabImage(const ImageMsg::SharedPtr msg)
+{
+    // Copy the ros image message to cv::Mat.
+    try
+    {
+        m_cvImPtr = cv_bridge::toCvCopy(msg);
+    }
+    catch (cv_bridge::Exception& e)
+    {
+        RCLCPP_ERROR(this->get_logger(), "cv_bridge exception: %s", e.what());
+        return;
+    }
+
+    // flip each frame before sending since our camera is mounted upside down.
+    cv::Mat flipped;
+    cv::flip(m_cvImPtr->image, flipped, -1); // -1 indicates both axes to flip
+    std::cout<<"one flipped frame has been sent"<<std::endl;
+
+    m_SLAM->TrackMonocular(flipped, Utility::StampToSec(msg->header.stamp));
+}
